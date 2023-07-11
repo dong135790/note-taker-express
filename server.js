843d413c6cf25874dfd89b073ea1e0e875bfa39e
Justin <dong135790@gmail.com>
2023-07-10 20:04:21 -0400

    moded * to very bottom

--- server.js
+++ server.js
@@ -35,15 +35,11 @@ app.get('/api/notes', (req, res) => {
 });
 
 // if you use '*', it must be on the bottom or it will override every click to index.html page
-app.get('*', (req, res) => {
-    // Serve the HTML page.
-    return res.sendFile(path.join(__dirname, 'public/index.html'));
-});
 
 // POST request to add new note
 app.post('/api/notes', (req, res) => {
     console.log(`Success! ${req.method} request received to add a note`)
-
+    
     // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
     // if all required properties are present
@@ -54,7 +50,7 @@ app.post('/api/notes', (req, res) => {
             text,
             review_id: uuid()
         };
-
+        
         let newFile = fs.readFileSync('./db/db.json');
         const notes = JSON.parse(newFile);
         notes.push(newNote);
@@ -80,6 +76,10 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+app.get('*', (req, res) => {
+    // Serve the HTML page.
+    return res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 // 127.0.0.1 is the same thing is localhost
 app.listen(PORT, () => {
